7b3dfac0a04728df8cf95ccfb746f55e37cb3582
Meris Bahtijaragic <meris.bahti@gmail.com>
2022-12-12 19:09:06 +0000

    fix(day10): start working

---
+++ src/main/scala/year2022/Day11.scala
@@ -0,0 +1,14 @@
+package year2022
+
+import java.text.ParseException
+
+object Day11 extends App {
+
+  def sol1(input: String): Int = {
+
+    val parsed = input.split("\n\n")
+    11
+  }
+  def sol2(input: String): Int = ???
+
+}
---
+++ src/test/scala/year2022/Day11Test.scala
@@ -0,0 +1,71 @@
+package year2022
+
+import scala.util.Using
+import scala.io.Source
+
+class Day11Test extends org.scalatest.funsuite.AnyFunSuite {
+  /* CHANGE THESE */
+  val inputFile = "src/test/scala/year2022/Day11-input.txt"
+  val testInput = """Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1"""
+  val Day = Day11
+  val skipTest = false
+
+  test("test input") {
+    if (!skipTest)
+      assert(Day.sol1(testInput) == 15)
+  }
+
+  test("test input sol2") {
+    if (!skipTest)
+      assert(Day.sol2(testInput) == 12)
+  }
+
+  test("real input") {
+    if (!skipTest) {
+      val res =
+        Using(Source.fromFile(inputFile)) { source =>
+          source.mkString
+        }
+      val input = res.get
+      assert(Day.sol1(input) === 13005)
+    }
+  }
+
+  test("real input part2") {
+    if (!skipTest) {
+      val res =
+        Using(Source.fromFile(inputFile)) { source =>
+          source.mkString
+        }
+      val input = res.get
+      assert(Day.sol2(input) === 11373)
+    }
+  }
+
+}
