fa1cdb86059d61eefa31269495f5d28cbdd79d7b
meris <meris.bahti@gmail.com>
2023-11-30 19:03:15 +0100

    prep for day1

---
+++ src/test/scala/year2023/DayXXTest.scala
@@ -0,0 +1,47 @@
+package year2023
+
+import scala.util.Using
+import scala.io.Source
+
+class DayXXTest extends org.scalatest.funsuite.AnyFunSuite {
+  def sol1(input: String): Int = ???
+  def sol2(input: String): Int = ???
+
+  val testInput = """"""
+
+  val dayName =
+    this.getClass().getName().split("\\.").last.replace("Test", "")
+  val inputFile = s"./${dayName}-input.txt"
+  val skipTest = dayName == "DayXX"
+
+  test("test input") {
+    if (!skipTest) assert(sol1(testInput) == -1)
+  }
+
+  test("test input sol2") {
+    if (!skipTest) assert(sol2(testInput) == -1)
+  }
+
+  test("real input") {
+    if (!skipTest) {
+      val res =
+        Using(Source.fromFile(inputFile)) { source =>
+          source.mkString
+        }
+      val input = res.get
+      assert(sol1(input) === -1)
+    }
+  }
+
+  test("real input part2") {
+    if (!skipTest) {
+      val res =
+        Using(Source.fromFile(inputFile)) { source =>
+          source.mkString
+        }
+      val input = res.get
+      assert(sol2(input) === -1)
+    }
+  }
+
+}
